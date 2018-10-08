5c57fc4d432763268d584a528941617d5379e5b5
xray <creator@darkmattercreations.com>
2018-10-08 16:09:54 -0500

    Iteration 2 Refactoring

--- Iteration-2/bowlinggame/game.py
+++ Iteration-2/bowlinggame/game.py
@@ -1,22 +1,32 @@
+STRIKE_VALUE = 10
+SPARE_VALUE = 10
+
 class Score:
     def __init__(self):
         self.score = 0
 
     def calculate(self, score_card):
         for _ in range(10):
-            print(score_card)
-            if score_card[0] == 10:
-                self.score += score_card[0] + score_card[1] + score_card[2]
-                score_card.pop(0)
+            if self.is_strike(score_card[0]):
+                self.score += STRIKE_VALUE + score_card[1] + score_card[2]
+                self.remove_values(score_card, 1)
 
-            elif score_card[0] + score_card[1] == 10:
-                self.score += score_card[0] + score_card[1] + score_card[2]
-                score_card.pop(0)
-                score_card.pop(0)
+            elif self.is_spare(score_card[0], score_card[1]):
+                self.score += SPARE_VALUE + score_card[2]
+                self.remove_values(score_card, 2)
 
             else:
                 self.score += score_card[0] + score_card[1]
-                score_card.pop(0)
-                score_card.pop(0)
+                self.remove_values(score_card, 2)
 
         return self.score
+
+    def is_strike(self, value):
+        return value == 10
+
+    def is_spare(self, value1, value2):
+        return value1 + value2 == 10
+
+    def remove_values(self, score_card, number_of_values):
+        for _ in range(number_of_values):
+            score_card.pop(0)
